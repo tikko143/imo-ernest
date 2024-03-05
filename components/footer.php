e50f5df3c50d427861467dff5f66516c41b9b3cd
Jasperr03 <jasperellevera03@gmail.com>
2024-03-05 15:51:38 +0800

    first practice

---
+++ components/footer.php
@@ -0,0 +1,5 @@
+<div class="footer">
+           <div class="footer-logo">
+                <img src="assets/images/Rockstar-Games-logo.jpg" alt="fakeimage.jpg">
+           </div>
+        </div>
---
+++ components/header.php
@@ -0,0 +1,9 @@
+<div class="header">
+            <h1 class="logo">Logo</h1>
+            <!--navlink-->
+            <div class="nav">
+                <a href="index.php">Home</a>
+                <a href="menu.php">Menu</a>
+                <a href="about.php">About</a>
+            </div>
+</div>
